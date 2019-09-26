6e21b2d4d034262eaf11bc6917e2a97b11b7a9d5
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-26 14:54:53 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
+  #your key/value pairs here!
   epic_tragedy = {
     :montague => {
+      :patriarch => {
+       }, :matriarch => {
+      }, :hero => {
+      }, :hero_friends => []
+    },
+     :capulet => {
       #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
+      :patriarch => {
+      }, :matriarch => {
+      }, :heroine => {
+      }, :heroine_friends => []
+      
       }}
-
-  
 end
